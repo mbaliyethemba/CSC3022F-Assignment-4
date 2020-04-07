c31e1eadf28250d1e60030f033d91d197bb74498
Themba <SHNMBA004@uct.ac.za>
2020-04-07 21:10:13 +0200

    Added the function for to remove all the comments from the ppm image

--- ImageCluster.cpp
+++ ImageCluster.cpp
@@ -1,1 +1,20 @@
+#include "ImageCluster.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
 
+using namespace std;
+
+void ImageReader(char * filename){
+	std::ifstream infile(filename, std::ios::in | std::ios::binary);
+	std::string line;
+	if(infile.is_open()){
+		getline(infile,line,'\n');
+		if(line == "P6"){
+		}
+	}
+}
+
+void comment(std::ifstream & file, std::string & line){
+	
+}
