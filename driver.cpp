0b26a9d5e80fa973c39e4073b113c256488a32db
Themba <SHNMBA004@uct.ac.za>
2020-05-02 17:32:03 +0200

    Added the file reader for the driver class

---
+++ driver.cpp
@@ -0,0 +1,26 @@
+#include "KMeansClusterer.h"
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sstream>
+#include <dirent.h>
+
+using namespace std;
+std::string *ppmImages;
+
+int main(int argc, char* argv[]){
+	SHNMBA004::KMeansClusterer k;
+	ppmImages = new string[100];
+	int s = 0;
+	DIR *fileDirectory;
+	struct dirent *entry;
+	if ( fileDirectory = opendir("Gradient_Numbers_PPMS/")){
+		while(entry = readdir(fileDirectory)){
+			if(strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..")){
+					ppmImages[s] = entry->d_name;
+					s++;
+					
+			}
+		}
+	}
+}
