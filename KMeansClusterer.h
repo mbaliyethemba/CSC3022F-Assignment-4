500a472b211c97414645fb505e5e6bb716956a4b
Themba <SHNMBA004@uct.ac.za>
2020-05-02 16:49:10 +0200

    Created the kmean cluster library

--- KMeansClusterer.h
+++ KMeansClusterer.h
@@ -1,1 +1,26 @@
+#ifndef KMEANSCLUSTERER_H
+#define KMEANSCLUSTERER_H
 
+#include <fstream>
+namespace SHNMBA004{
+	class KMeansClusterer {
+		private:
+			std::string magicNumber; // A "magic number" for identifying the file type
+			int width; // Width of the image
+			int height; // Height of the image
+			int maxColorVal; // Maximum color value
+			char *dataPointer; // A series of rows and columns (raster) that stores important binary
+			int *histogram;
+			int *buff;
+			int *binArray;
+			int val;
+			std::string *ppmImages;
+			int **ppmHist;
+			
+		public:
+			KMeansClusterer();
+			~KMeansClusterer();
+			void ImageReader(std::string filename);
+	};
+}
+#endif
