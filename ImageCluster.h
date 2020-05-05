32b3821ab16555094456f7262bfcc4e52f8d53ad
Themba <SHNMBA004@uct.ac.za>
2020-05-06 00:53:24 +0200

    Added the final touches to the program

--- ImageCluster.h
+++ ImageCluster.h
@@ -2,17 +2,18 @@
 #define IMAGECLUSTER_H
 
 #include <fstream>
-
-using namespace std;
-
-class ImageCluster{
-	int rows;
-	int cols;
-	int intensity;
-	public:
-		//function to read the ppm image
-		void ImageReader(char * filename);
-		void comment(std::ifstream & file, std::string & line);
-};
+namespace SHNMBA004{
+	class ImageCluster{
+		private:
+			int rows;
+			int cols;
+			int intensity;
+		
+		public:
+			//function to read the ppm image
+			void ImageReader(char * filename);
+			void comment(std::ifstream & file, std::string & line);
+	};
+}
 
 #endif
