10f8d1d37fe61d4d69622a25a3779a53a1dc1696
Themba <SHNMBA004@uct.ac.za>
2020-05-06 00:55:19 +0200

    Added the if statements to save the functions appropriately

--- driver.cpp
+++ driver.cpp
@@ -6,27 +6,15 @@
 #include <dirent.h>
 
 using namespace std;
-std::string *ppmImages;
 
 int main(int argc, char* argv[]){
 	SHNMBA004::KMeansClusterer k;
-	ppmImages = new string[100];
-	int s = 0;
-	DIR *fileDirectory;
-	struct dirent *entry;
-	if ( fileDirectory = opendir("Gradient_Numbers_PPMS/")){
-		while(entry = readdir(fileDirectory)){
-			if(strcmp(entry->d_name,".") != 0 && strcmp(entry->d_name,"..")){
-					ppmImages[s] = entry->d_name;
-					s++;
-					
-			}
-		}
+	if(argc < 2){
+		std::cout << "Not enough parameters" << std::endl;
 	}
-	for(int i = 0; i < 100; i++){
-		k.ppmReader(ppmImages[i]);
-		k.grayscale();
-		k.charToInt();
-		k.hist();
-	}
+	k.ReadingList();
+	k.centriod(10);
+	k.centriodArray();
+	k.load_distances();
+	k.clusterprint();
 }
